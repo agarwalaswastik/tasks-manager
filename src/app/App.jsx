6c2f36fbb93a3844808108511029bbbb00eb0816
agarwalaswastik <agarwalaswastik@gmail.com>
2023-07-10 02:31:29 +0530

    Added persistence using localStorage

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,18 @@
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { store, persistor } from "./store";
 import Navbar from "./Navbar";
 import TaskBoard from "../features/tasks/TaskBoard";
+import { PersistGate } from "redux-persist/integration/react";
 
 const App = () => {
     return (
         <Provider store={store}>
-            <Navbar />
-            <main className="absolute bottom-0 left-0 right-0 top-24 p-2 -z-10">
-                <TaskBoard />
-            </main>
+            <PersistGate loading={null} persistor={persistor}>
+                <Navbar />
+                <main className="absolute bottom-0 left-0 right-0 top-24 -z-10 p-2">
+                    <TaskBoard />
+                </main>
+            </PersistGate>
         </Provider>
     );
 };
--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import filtersReducer from "../features/filters/filtersSlice";
 import searchReducer from "../features/search/searchSlice";
 import tasksReducer from "../features/tasks/tasksSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const rootReducer = combineReducers({
+    filters: filtersReducer,
+    search: searchReducer,
+    tasks: tasksReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        filters: filtersReducer,
-        search: searchReducer,
-        tasks: tasksReducer,
-    },
+    reducer: persistedReducer,
+    middleware: [thunk],
 });
+
+export const persistor = persistStore(store);
